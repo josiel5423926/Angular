28110bdfa57975c53598a3639685adb3bd1a32ba
Josiel Souza <josielsouza.dj@gmail.com>
2022-11-13 13:21:39 -0300

    create pages products and componets views

--- crud/frontend/src/app/app-routing.module.ts
+++ crud/frontend/src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ProductCreateComponent } from "./components/product/product-create/product-create.component";
+import { HomeComponent } from "./views/home/home.component";
+import { ProductCrudComponent } from "./views/product-crud/product-crud.component";
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent,
+  },
+  {
+    path: "products",
+    component: ProductCrudComponent,
+  },
+  {
+    path: "products/create",
+    component: ProductCreateComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
